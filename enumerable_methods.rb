f7b59669a950db4f9bc65b06577887cc18966e1b
Ahmed-Benj <ahmedblaugrana@gmail.com>
2020-12-20 23:30:12 +0100

    add my_each_with_index method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -9,6 +9,18 @@ def my_each (array)
   end
 end
 
+def my_each_with_index (array)
+  iterator = 0
+  while iterator < array.length
+
+    yield(array[iterator],iterator)
+
+    iterator += 1
+
+  end
+end
+
 friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
 
 my_each(friends) { |friend| puts friend }
+my_each_with_index(friends) { |friend, index| puts friend + index.to_s }
