cc2108190cf3c9a5df804b4bbf0071924e9389eb
Ahmed-Benj <ahmedblaugrana@gmail.com>
2020-12-22 00:36:06 +0100

    add my_select function

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -20,7 +20,20 @@ def my_each_with_index (array)
   end
 end
 
+def my_select (input_array)
+ output_array = []
+ my_each(input_array) do |element|
+ if yield(element)
+  output_array.push(element)
+ end
+ end
+print output_array
+end
+
+
+
 friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
 
-my_each(friends) { |friend| puts friend }
-my_each_with_index(friends) { |friend, index| puts friend + index.to_s }
+#my_each(friends) { |friend| puts friend }
+#my_each_with_index(friends) { |friend, index| puts friend + index.to_s }
+my_select(friends) {|friend| friend != "Brian"}
