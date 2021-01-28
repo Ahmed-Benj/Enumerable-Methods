841bafd611c649a4b81cf430af9b47e88ae31d86
Ahmed-Benj <ahmedblaugrana@gmail.com>
2021-01-28 08:42:53 +0100

    add enumerable method 13

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -22,84 +22,86 @@ module Enumerable
     end
   end
 
-def my_select
- output_array = []
- self.my_each do |element|
- if yield(element)
-  output_array.push(element)
- end
- end
-print output_array
+  def my_select
+    output_array = []
+    self.my_each do |element|
+    if yield(element)
+      output_array.push(element)
+    end
+    end
+    print output_array
+  end
+
+  def my_all
+    output_array = []
+    self.my_each do |element|
+    if yield(element)
+    output_array.push(element)
+    end
+    end
+    result = output_array.length == self.length
+  print result
+  end
+  
+  def my_any 
+    result = false
+    self.my_each do |element|
+    if yield(element)
+    result = true
+    print result
+    exit
+    end
+    end
+  print result
+  end
+
+  def my_none
+    output_array = []
+    self.my_each do |element|
+    if yield(element)
+    output_array.push(element)
+    end
+    end
+    result = output_array.length == 0
+  print result
+  end
+
+  def my_count
+    i = 0
+    self.my_each do |element|
+      i += 1
+    end
+    print i
+  end
+
+  def my_map
+    output_array = []
+    self.my_each do |element|
+    if yield(element)
+      output_array.push(element)
+    end
+    end
+  print output_array
+  end
+
+  def my_map_proc(proc_arg)
+    output_array = []
+    self.my_each do |element|
+      output_array.push(proc_arg.call(element))
+    end
+    print output_array
+  end
+
+  def my_inject
+    sum = self[0]
+    temp = self.shift
+    self.my_each do |element|
+      sum = yield(sum,element)
+    end
+    print sum
+  end
+
 end
-
-
-
-def my_all
-  output_array = []
-  self.my_each do |element|
-  if yield(element)
-   output_array.push(element)
-  end
-  end
-  result = output_array.length == self.length
- print result
- end
- 
-def my_any 
-  result = false
-  self.my_each do |element|
-  if yield(element)
-   result = true
-   print result
-   exit
-  end
-  end
- print result
-end
-
- def my_none
-  output_array = []
-  self.my_each do |element|
-  if yield(element)
-   output_array.push(element)
-  end
-  end
-  result = output_array.length == 0
- print result
- end
-
-def my_count
- i = 0
- self.my_each do |element|
-  i += 1
- end
- print i
-end
-
-def my_map
-  output_array = []
-  self.my_each do |element|
-  if yield(element)
-   output_array.push(element)
-  end
-  end
- print output_array
-end
-
-def my_inject
-  sum = self[0]
-  temp = self.shift
-  self.my_each do |element|
-    sum = yield(sum,element)
-  end
-  print sum
-end
-
-end
-
-friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
-
-friends = [3,4,5]
 
 def multiply_els(array)
   array.my_inject do |sum, friend|
@@ -107,15 +109,30 @@ def multiply_els(array)
   end
 end 
 
+
+friends_strings = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+friends_ints = [3,4,5]
+
 #friends.my_each { |friend| puts friend }
-#my_each(friends) { |friend| puts friend }
 #friends.my_each_with_index { |friend, index| puts friend + index.to_s }
 #friends.my_select {|friend| friend != "Brian"}
+#friends_strings.my_all {|friend| friend.length > 5}
+#friends_strings.my_any {|friend| friend.length == 2}
+#friends_strings.my_none{|friend| friend.length > 5}
+#friends_ints.my_count
+#friends_strings.my_map {|friend| friend.length > 5}
+#friends.my_inject {|sum, friend| sum + friend}
 
-#friends.my_all {|friend| friend.length > 5}
-#my_any(friends) {|friend| friend.length == 2}
-#my_none(friends){|friend| friend.length > 5}
-#my_count(friends)
-#my_map(friends) {|friend| friend.length > 5}
-friends.my_inject {|sum, friend| sum + friend}
+#12
 #multiply_els(friends)
+
+#13
+proc = Proc.new do |friend| 
+  if friend.length > 5 then
+     friend 
+  else
+     print "nothing to push"
+  end
+end
+friends_strings.my_map_proc(proc)
