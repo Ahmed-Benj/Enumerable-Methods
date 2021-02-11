2af4b1c495282c2966246822bfca53b9d07c767c
Ahmed-Benj <ahmedblaugrana@gmail.com>
2021-02-12 00:49:36 +0100

    fix my_select and my_all

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -15,30 +15,45 @@ module Enumerable
 
   # 4
   def my_each_with_index
-    iterator = 0
-    while iterator < length
-
-      yield(self[iterator], iterator)
-
-      iterator += 1
-
+    if block_given?
+      iterator = 0
+      while iterator < length
+        yield(self[iterator], iterator)
+        iterator += 1
+      end
+    else
+      to_enum(__method__)
     end
   end
 
   # 5
   def my_select
-    output_array = []
-    my_each do |element|
-      output_array.push(element) if yield(element)
+    if block_given?
+      output_array = []
+      my_each do |element|
+        output_array.push(element) if yield(element)
+      end
+      print output_array
+    else
+      to_enum(__method__)
     end
-    print output_array
   end
 
   # 6
-  def my_all
+  def my_all(pattern = nil)
     output_array = []
-    my_each do |element|
-      output_array.push(element) if yield(element)
+    if pattern and !block_given?
+      my_each do |element|
+        output_array.push(element) if pattern === element
+      end
+    elsif block_given?
+      my_each do |element|
+        output_array.push(element) if yield(element)
+      end
+    elsif !block_given?
+      my_each do |obj|
+        output_array.push(obj) if obj != false and obj != nil
+      end
     end
     result = output_array.length == length
     print result
@@ -133,23 +148,33 @@ end
 
 # input section
 
- friends = %w[Sharon Leo Leila Brian Arun]
+friends = %w[Sharon Leo Leila Brian Arun]
 
-# friends_strings = %w[Sharon Leo Leila Brian Arun]
+ friends_strings = %w[Sharon Leo Leila Brian Arun]
 
 # friends_ints = [3, 4, 5]
 
 # 3
- friends.my_each #{ |friend| puts friend }
+# friends.my_each { |friend| puts friend }
 
 # 4
 # friends.my_each_with_index { |friend, index| puts friend + index.to_s }
+# hash = Hash.new
+# %w(cat dog wombat).my_each_with_index { |item, index| hash[item] = index }
+# p hash
 
 # 5
 # friends.my_select {|friend| friend != "Brian"}
+# [1,2,3,4,5].my_select { |num|  num.even?  }
 
 # 6
 # friends_strings.my_all {|friend| friend.length > 5}
+ puts %w[ant bear cat].my_all { |word| word.length >= 3 }
+ puts %w[ant bear cat].my_all { |word| word.length >= 4 }
+ puts %w[ant bear cat].my_all(/t/)
+ puts [1, 2i, 3.14].my_all(Numeric)
+ puts [nil, true, 99].my_all
+ puts [].my_all
 
 # 7
 # friends_strings.my_any {|friend| friend.length == 2}
