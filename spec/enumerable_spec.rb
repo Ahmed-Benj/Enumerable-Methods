cb89b6bd492c4a31d2dd39ba2ee06d46aee60e2a
ahmed BEN-JMII <ahmed.ben-jmii@talan.com>
2021-06-17 23:30:06 +0100

    test my_each_with_index

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,10 +1,23 @@
 require_relative '../enumerable_methods'
 
 describe Enumerable do
+  let(:ints) { [3, 4, 5] }
+  friends = %w[Sharon Leo Leila Brian Arun]
+  friends_strings = %w[Sharon Leo Leila Brian Arun]
+
   describe '#my_each' do
-    let(:ints) { [3, 4, 5] }
     it 'Executes a block of code' do
       expect(ints.my_each { |x| x * 5 }).to eq(ints.each { |x| x * 5 })
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it 'Executes a block of code' do
+      expect(friends.my_each_with_index do |friend, index|
+               puts friend + index.to_s
+             end).to eq(friends.each_with_index do |friend, index|
+                          puts friend + index.to_s
+                        end)
     end
   end
 end
